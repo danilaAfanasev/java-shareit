c80255916327cf9570b774bd8bffcd824dfffd86
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-12 16:04:01 +0300

    Изменен класс ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 /**
  * TODO Sprint add-controllers.
  */
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private Boolean available;
+    private Long requestId;
 }
