32494d9e4899b5904f55d46a1052ca076e677411
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-12 16:06:52 +0300

    Создан класс UserDto

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+/**
+ * TODO Sprint add-controllers.
+ */
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+}
