2ebdcad0a7d6a0d7ab834c9d19d524248c6a2f37
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-12 16:02:31 +0300

    Изменен класс BookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
 /**
- * TODO Sprint add-bookings.
+ * TODO Sprint add-controllers.
  */
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
 public class BookingDto {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long itemId;
+    private Long bookerId;
+    private String status;
 }
