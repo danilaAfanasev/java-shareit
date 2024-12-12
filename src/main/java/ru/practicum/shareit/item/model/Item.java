ba06d1cd97d0d75633f6fd1adc48dd8c4bd8179c
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-12 16:04:09 +0300

    Изменен класс Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 /**
  * TODO Sprint add-controllers.
  */
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
 public class Item {
+    private Long id;
+
+    @NotBlank
+    private String name;
+    private String description;
+    private Boolean available;
+    private Long ownerId;
+    private Long requestId;
 }
