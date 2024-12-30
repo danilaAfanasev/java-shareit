706aa5a4abad78d325ddaa7d92e9ba7b1c7913ad
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-30 05:15:42 +0300

    Полностью переработан пакет user

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,21 +1,25 @@
 package ru.practicum.shareit.user.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder(toBuilder = true)
+@Entity
+@Table(name = "users")
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Email
-    @NotBlank
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
