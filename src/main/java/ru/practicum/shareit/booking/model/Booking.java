a129d8332899c675820b073f0d08fe2b94410bca
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-30 05:13:52 +0300

    Полностью переработан пакет booking

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,15 +1,40 @@
 package ru.practicum.shareit.booking.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "bookings")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "start_date", nullable = false)
     private LocalDateTime start;
+
+    @Column(name = "end_date", nullable = false)
     private LocalDateTime end;
-    private Long itemId;
-    private Long bookerId;
-    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id")
+    private User booker;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
 }
