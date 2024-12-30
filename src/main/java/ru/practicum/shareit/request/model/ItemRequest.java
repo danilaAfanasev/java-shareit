718a07c5b9cc3bf9047478bcca8c62cfdcaa098b
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-12-30 05:15:31 +0300

    Полностью переработан пакет request

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,14 +1,32 @@
 package ru.practicum.shareit.request.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import ru.practicum.shareit.user.model.User;
+import java.time.LocalDate;
 
-@Builder(toBuilder = true)
+@Entity
+@Table(name = "requests")
 @Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "description")
     private String description;
-    private String requesterName;
-    private LocalDateTime created;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "requester_id", referencedColumnName = "id")
+    private User creator;
+
+    @Column(name = "created")
+    private LocalDate created;
 }
